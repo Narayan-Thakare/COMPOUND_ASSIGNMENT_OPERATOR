5146bf0e9b561daec1ab7b8c3a17a290e299dbfe
Narayan Thakare <narayan111thakare@gmail.com>
2024-07-24 16:34:49 +0530

    first commit

---
+++ src/CompoundAssigment.java
@@ -0,0 +1,41 @@
+
+public class CompoundAssigment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		{
+			
+			int salary = 10000;
+			salary += 6786;
+			
+			System.out.println(salary);
+		}
+			{
+				int salary = 10000;
+				salary -= 8788;
+				System.out.println(salary);
+			}
+			{
+				int salary = 10000;
+				salary *=  9000;
+				System.out.println(salary);
+			}
+			{
+				int salary = 10000;
+				salary /= 2000;
+				System.out.println(salary);
+			}
+			{
+				int salary = 14000;
+				salary %= 2000;
+				System.out.println(salary);
+			}
+		
+		
+		
+		
+	}
+
+}
