fd97ef0e9a24321a7329ce89f5e84cd52c951b4d
Narayan Thakare <narayan111thakare@gmail.com>
2024-07-24 23:34:30 +0530

    second commit

--- src/CompoundAssigment.java
+++ src/CompoundAssigment.java
@@ -4,38 +4,34 @@ public class CompoundAssigment {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 		{
-			
+
 			int salary = 10000;
 			salary += 6786;
-			
-			System.out.println(salary);
+
+			System.out.println("This is for addition :" + salary);
 		}
-			{
-				int salary = 10000;
-				salary -= 8788;
-				System.out.println(salary);
-			}
-			{
-				int salary = 10000;
-				salary *=  9000;
-				System.out.println(salary);
-			}
-			{
-				int salary = 10000;
-				salary /= 2000;
-				System.out.println(salary);
-			}
-			{
-				int salary = 14000;
-				salary %= 2000;
-				System.out.println(salary);
-			}
-		
-		
-		
-		
+		{
+			int salary = 10000;
+			salary -= 8788;
+			System.out.println("This is for subtract :" + salary);
+		}
+		{
+			int salary = 10000;
+			salary *= 9000;
+			System.out.println("This is for multyplication :" + salary);
+		}
+		{
+			int salary = 10000;
+			salary /= 2000;
+			System.out.println("This is for division :" + salary);
+		}
+		{
+			int salary = 14000;
+			salary %= 2000;
+			System.out.println("This is for modules: " + salary);
+		}
+
 	}
 
 }
