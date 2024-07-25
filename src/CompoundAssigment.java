cab5d04885dec6b30b1542b63386cf8fe8f6c702
Narayan Thakare <narayan111thakare@gmail.com>
2024-07-25 19:09:17 +0530

    first commit

--- src/CompoundAssigment.java
+++ src/CompoundAssigment.java
@@ -9,27 +9,27 @@ public class CompoundAssigment {
 			int salary = 10000;
 			salary += 6786;
 
-			System.out.println("This is for addition :" + salary);
+			System.out.println("This is for addition  :" + salary);
 		}
 		{
 			int salary = 10000;
 			salary -= 8788;
-			System.out.println("This is for subtract :" + salary);
+			System.out.println("This is for subtract  :" + salary);
 		}
 		{
 			int salary = 10000;
 			salary *= 9000;
-			System.out.println("This is for multyplication :" + salary);
+			System.out.println("This is for multyplication  :" + salary);
 		}
 		{
 			int salary = 10000;
 			salary /= 2000;
-			System.out.println("This is for division :" + salary);
+			System.out.println("This is for division  :" + salary);
 		}
 		{
 			int salary = 14000;
 			salary %= 2000;
-			System.out.println("This is for modules: " + salary);
+			System.out.println("This is for modules  : " + salary);
 		}
 
 	}
